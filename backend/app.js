ae98f58a48603990e14b3a6f1076788d4bb22e58
Nisal1990 <35098989+Nisal1990@users.noreply.github.com>
2023-10-24 13:39:41 +0530

    backend integration

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,13 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
 
+// import routes 
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
 // database connection 
 mongoose.connect(process.env.DATABASE, {
      useNewUrlParser: true,
@@ -15,9 +22,30 @@ mongoose.connect(process.env.DATABASE, {
 }).then(()=>console.log("DB connected"))
 .catch((err)=>console.log(err));
 
+// Middleware 
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+// Routes middleware
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+// error middleware 
+app.use(errorHandler);
 //port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 9000
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
+
+// 20:04
